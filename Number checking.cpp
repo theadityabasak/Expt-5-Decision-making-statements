5e165181bdde15d35780a208a84985bd59e72bf0
theadityabasak <orandom46@gmail.com>
2024-11-05 19:20:17 +0530

    Add files via upload

---
+++ Number checking.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int i,j,a[5]
+    cout<<"Enter a number";
+    cin>>n;
+    cout<<"enter 5 numbers";
+    for(i=0;i<=4;i++)
+    { 
+        cin>>a[i];
+    }
+    for(i=0;i<5;i++)
+    {
+        if(a[i]==n){
+            p=i;
+        }
+    }
+    cout<<"User inputted number is in"<<p<<"th position";
+}
+*/
+Enter a number24
+enter 5 numbers56
+53
+24
+78
+89
+User inputted number is in2th position
+*/
